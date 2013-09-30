07e8e0dc8799848583db3f5164fc9c80aef4e288
Guillaume Massé <masgui@gmail.com>
2013-09-29 21:39:17 -0400

    app autocomplete bar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
-app.controller('MainCtrl', function ($scope, permissions) {
-	$scope.permissions = permissions.all();
+// $scope.permissions = permissions.all(); permissions
+app.controller('MainCtrl', function ($scope, androidapps) {
+	$scope.apps = [];
+	$scope.selected = null;
+
+	$scope.search = function(term) {
+		$scope.apps = androidapps.query(term);
+	};
+
+	$scope.select = function(item) {
+		$scope.selected = item;
+		$scope.term = item.name;
+	};
 });
