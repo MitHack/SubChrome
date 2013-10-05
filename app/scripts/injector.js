4358be9ddc24da12ad83d216be5e54d5c1351e7a
Guillaume Masse <masgui@gmail.com>
2013-10-05 17:26:38 -0400

    match base + specific site

--- app/scripts/injector.js
+++ app/scripts/injector.js
@@ -1,9 +1,10 @@
-$(document).ready(function() {
-  console.log("YO");
-  $("body").css("background-color", "#FF0000");
+// $(document).ready(function() {
+//   console.log("YO");
+//   $("body").css("background-color", "#FF0000");
   
-  window.setTimeout(function() {
-    var composeBtn = $("#fbNotificationsJewel").children("a")[0];
-    composeBtn.click();
-  }, 100);
-});
+//   window.setTimeout(function() {
+//     var composeBtn = $("#fbNotificationsJewel").children("a")[0];
+//     composeBtn.click();
+//   }, 100);
+// });
+alert("basejs");
