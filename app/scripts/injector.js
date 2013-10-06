379fd767dd56ed4993e62e617ead8c858f251414
Guillaume Masse <masgui@gmail.com>
2013-10-06 03:56:45 -0400

    avoid clash with twitter search

--- app/scripts/injector.js
+++ app/scripts/injector.js
@@ -8,7 +8,7 @@
 	var inject = hereDoc(function() {
 	/*!
 <div ng-controller="MainCtrl" id='SubChrome'>
-	<typeahead class="search" items="commands" term="term" search="search(term)" select="select(item)">
+	<typeahead class="qsearch" items="commands" term="term" search="search(term)" select="select(item)">
 	  <ul class="dropdown-menu" role="menu">
 	    <li typeahead-item="command" ng-repeat="command in commands" role="presentation" class="results">
 	       <a href="#" role="menuitem" class="name">{{ command.name }}</a>
