7911fa6c5e23f7a49fdd3808876222b288b738a1
Guillaume Masse <masgui@gmail.com>
2013-10-06 02:49:37 -0400

    add picture, location and tweet now

--- app/scripts/sites/twitter.js
+++ app/scripts/sites/twitter.js
@@ -2,35 +2,14 @@ var site = {
   name: "Twitter",
   searchurl: "/search?q=",
   commands: [
-    {
-      name: "Home",
-      targetEl: "#global-nav-home a"
-    },
-    {
-      name: "Connect",
-      targetEl: "[role='navigation'] [title='Connect']",
-    },
-    {
-      name: "Discover",
-      targetEl: "[role='navigation'] [title='Discover']",
-    },
-    {
-      name: "Me",
-      targetEl: "[role='navigation'] [title='Me']",
-    },
-    {
-      name: "Settings and help",
-      targetEl: "a#user-dropdown-toggle"    
-    },
-    /*{
-      name: "Compose new Tweet",
-      targetEl: "#tweet-box-mini-home-profile",
-      focusEl: "#tweet-box-mini-home-profile"
-    },*/
-    {
-      name: "Compose new Tweet",
-      targetEl: "#global-new-tweet-button",
-      focusEl: "#tweet-box-global"   
-    } 
+    { name: "Compose new Tweet", targetEl: "#global-new-tweet-button", focusEl: "#tweet-box-global" },
+    { name: "Send Tweet", targetEl: "#global-tweet-dialog-dialog button:contains('Tweet')" },
+    { name: "Add a picture", targetEl: "#global-tweet-dialog-dialog [type='file']" },
+    { name: "Add a location", targetEl: "#global-tweet-dialog-dialog button[title='Add location']" },
+    { name: "Home", targetEl: "#global-nav-home a" },
+    { name: "Connect", targetEl: "[role='navigation'] [title='Connect']" },
+    { name: "Discover", targetEl: "[role='navigation'] [title='Discover']" },
+    { name: "Me", targetEl: "[role='navigation'] [title='Me']" },
+    { name: "Settings and help", targetEl: "a#user-dropdown-toggle" },
   ]
 };
