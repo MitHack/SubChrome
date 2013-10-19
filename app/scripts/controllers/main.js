4600dd2ae8e8f1efe6e3abbe793870d2a825d11b
Guillaume Masse <masgui@gmail.com>
2013-10-19 01:14:19 -0400

    use anchors to search :-)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,55 +1,23 @@
 'use strict';
 
 app.controller('MainCtrl', function ($scope, $timeout, $window) {
-
-	$scope.commands = typeof site !== "undefined" ? site.commands : null;
+	$scope.commands = [];
 	$scope.command = null;
 	$scope.term = "";
 
-	function findElem(command){
-		var elem = null;
-		if(_.isArray(command.targetEl)){
-			_.find(command.targetEl, function(selector){
-				var e = $(selector);
-				if(e.length !== 0){
-					elem = e;
-					return true;
-				}
-				return false;
-			})
-		} else {
-			elem = $(command.targetEl);
-		}
-		return elem;
-	}
-
 	$scope.search = function(term) {
-        if (typeof site === "undefined") return;
-
-		$scope.commands = _.filter(site.commands, function(command){
-			return command.name.toLowerCase().indexOf(term.toLowerCase()) !== -1 &&
-				0 !== findElem(command).length;
+		$scope.commands = _.filter(document.getElementsByTagName("a"),function(a){ 
+			return a.innerText.toLowerCase().indexOf(term.toLowerCase()) !== -1 && a.innerText !== ""
 		})
-		if(term !== "") {
-			$scope.commands = $scope.commands.concat({
-	      		name: "Search for \"" + term + "\"",
-	      		type: "search"
-    		});
-		}
 	};
 
 	$scope.select = function(item) {
-		if(item.type == "search") {
-			$window.location = site.searchurl + encodeURIComponent($scope.term);
-			return;
-		}
-
 		$scope.command = item;
 		$scope.term = "";
 		
-		var elem = findElem(command);
+		var elem = $(item);
 
-		if(0 !== elem.length){
+		if(0 == elem.length){
 			return;
 		}
 
@@ -64,12 +32,6 @@ app.controller('MainCtrl', function ($scope, $timeout, $window) {
 		if(elem.length) {
 			elem[0].click();
 			highlight(elem);
-		}
-		if(item.focusEl){
-			highlight($(item.focusEl));
-			$timeout(function(){
-				$(item.focusEl).focus();
-			},400);
 		}
 	};
 });
